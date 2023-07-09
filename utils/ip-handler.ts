4e6f55c26213e8ad193e568a53102968a32888f5
HAKJOON0223 <2hakjoon@gmail.com>
2023-07-09 18:48:54 +0900

    test

--- utils/ip-handler.ts
+++ utils/ip-handler.ts
@@ -3,23 +3,22 @@ export class IPHandler {
 
   constructor() {
     this.ipObj = {};
+    setInterval(() => {
+      for (let ip of Object.keys(this.ipObj)) {
+        if (this.ipObj[ip]) {
+          this.ipObj[ip] = this.ipObj[ip] - 1;
+        }
+        if (this.ipObj[ip] === 0) {
+          delete this.ipObj[ip];
+        }
+      }
+    }, 3333);
   }
 
   connect(ip: string) {
     if (this.ipObj[ip] > 2) return false;
 
-    this.ipObj[ip] = this.ipObj[ip] === undefined ? 0 : this.ipObj[ip] + 1;
-    setTimeout(() => {
-      if (!this.ipObj[ip]) {
-        return;
-      }
-      if (this.ipObj[ip]) {
-        this.ipObj[ip] = this.ipObj[ip] - 1;
-      }
-      if (this.ipObj[ip] === 0) {
-        delete this.ipObj[ip];
-      }
-    }, 10000);
+    this.ipObj[ip] = this.ipObj[ip] === undefined ? 1 : this.ipObj[ip] + 1;
     return true;
   }
 
